31e819e8583321b8428a1d119f41ca2e7acb3276
MarinSenzzy <124504489+MarinSenzzy@users.noreply.github.com>
2026-01-19 11:21:49 +0000

    Deploying to gh-pages from @ MarinSenzzy/goit-js-hw-11@0ecc65ea07b0985b097551222ffbc4202cf00008 🚀

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import{a,i as n}from"./assets/vendor-rn7jRtGp.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const u="54255948-743bb2e2e8edc610c07792dbd",d="https://pixabay.com";a.defaults.baseURL=d;function f(s){return a.get("/api/",{params:{q:s||"",image_type:"photo",key:u,orientation:"horizontal",safesearch:"true"}}).then(function(r){return r.data.total===0&&n.error({message:"Sorry, there are no images matching your search query. Please try again!",closeOnClick:!0,close:!1,position:"topRight",transitionIn:"fadeInLeft",transitionOut:"fadeOutLeft"}),r.data.hits}).catch(function(r){console.log(r)})}const p=document.querySelector(".gallery"),m=s=>s.map(({webformatURL:r,largeImageURL:i,tags:o,likes:e,views:t,comments:c,downloads:l})=>`
+import{i as f,a,S as p}from"./assets/vendor-BkVuWn-o.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const l of s.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function r(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(t){if(t.ep)return;t.ep=!0;const s=r(t);fetch(t.href,s)}})();const n=o=>{f.error({message:o,closeOnClick:!0,close:!1,position:"topRight",transitionIn:"fadeInLeft",transitionOut:"fadeOutLeft"})},m="54255948-743bb2e2e8edc610c07792dbd",g="https://pixabay.com";a.defaults.baseURL=g;function y(o){return a.get("/api/",{params:{q:o||"",image_type:"photo",key:m,orientation:"horizontal",safesearch:"true"}}).then(function(e){return e.data.total===0&&n("Sorry, there are no images matching your search query. Please try again!"),e.data.hits}).catch(function(e){return e.response?(n(e.response.status),console.log(e.response.data),console.log(e.response.status),console.log(e.response.headers)):e.request?(n("error request"),console.log(e.request)):(n(e.message),console.log("Error",e.message)),[]})}const c=document.querySelector(".gallery"),u=document.querySelector(".loader"),h=o=>o.map(({webformatURL:e,largeImageURL:r,tags:i,likes:t,views:s,comments:l,downloads:d})=>`
   <li class="gallery-item">
-  <a class="gallery-link" href="${i}">
+  <a class="gallery-link" href="${r}">
   <img
   class="gallery-image"
-  src="${r}"
-  alt="${o}"
+  src="${e}"
+  alt="${i}"
   />
   </a>
    <ul class="discriptions">
       <li>
         <p class="disc-title">likes</p>
-        <p class="disc-text">${e}</p>
-      </li>
-      <li>
-        <p class="disc-title">views</p>
         <p class="disc-text">${t}</p>
       </li>
       <li>
+        <p class="disc-title">views</p>
+        <p class="disc-text">${s}</p>
+      </li>
+      <li>
         <p class="disc-title">comments</p>
-        <p class="disc-text">${c}</p>
+        <p class="disc-text">${l}</p>
       </li>
       <li>
         <p class="disc-title">downloads</p>
-        <p class="disc-text">${l}</p>
+        <p class="disc-text">${d}</p>
       </li>
     </ul>
   </li>
-  `).join(" ");function y(s){p.insertAdjacentHTML("beforeend",m(s))}const g=document.querySelector(".form");g.addEventListener("submit",h);function h(s){s.preventDefault();const r=s.target,{"search-text":{value:i}}=r.elements;if(!i.trim()){console.log("empty input!");return}f(i.trim()).then(o=>{y(o)})}
+  `).join(" ");function L(o){c.insertAdjacentHTML("beforeend",h(o)),new p(".gallery a").refresh()}function b(){c.innerHTML=""}function q(){u.style.display="inline-block"}function O(){u.style.display="none"}const S=document.querySelector(".form");S.addEventListener("submit",x);function x(o){o.preventDefault();const e=o.target,{"search-text":{value:r}}=e.elements;if(!r.trim()){console.log("empty input!"),n("Please fill in the search field!");return}b(),q(),y(r.trim()).then(i=>{O(),L(i)})}
 //# sourceMappingURL=index.js.map
